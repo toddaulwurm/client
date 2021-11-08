14f2f6cf1acd74f9fef599c289098a0f6a034f6b
toddaulwurm <85901690+toddaulwurm@users.noreply.github.com>
2021-11-08 13:45:53 -0600

    1

---
+++ src/Components/ProductForm.js
@@ -0,0 +1,30 @@
+import React,{useState} from "react";
+import axios from "axios";
+import './ProductForm.css';
+
+export default ()=>{
+    const [title,setTitle] = useState('');
+    const [price, setPrice] = useState(0);
+    const [description, setDescription] = useState('');
+
+    const onSubmitHandler = e =>{
+        e.preventDefault();
+        axios.post('http://localhost:8000/api/products',{
+            title,price,description
+        })
+        .then(res=>console.log(res))
+        .catch(err=>console.log(err))
+    }
+
+    return(
+        <form onSubmit={onSubmitHandler} class="flexer">
+            <label>Title:</label>
+            <input type='text' onChange={(e)=>{setTitle(e.target.value)}} value={title}/><br/>
+            <label>Price:</label>
+            <input type='text' onChange={(e)=>{setPrice(e.target.value)}} value={price}/><br/>
+            <label>Description:</label>
+            <input type='text' onChange={(e)=>{setDescription(e.target.value)}} value={description}/><br/>
+            <input type='submit' value='Add Product'/>
+        </form>   
+    )
+}

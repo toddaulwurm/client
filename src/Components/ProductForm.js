798c67dd044e67592a9ea5ccc595e134c635e321
toddaulwurm <85901690+toddaulwurm@users.noreply.github.com>
2021-11-09 13:33:14 -0600

    validation

--- src/Components/ProductForm.js
+++ src/Components/ProductForm.js
@@ -6,6 +6,38 @@ export default ()=>{
     const [title,setTitle] = useState('');
     const [price, setPrice] = useState(0);
     const [description, setDescription] = useState('');
+    const [titleError, setTitleError] = useState('');
+    const [priceError, setPriceError] = useState('');
+    const [descriptionError, setDescriptionError] = useState('');
+
+
+    const handleTitle=e=>{
+        setTitle(e.target.value)
+        if(e.target.value.length>2){
+            setTitleError('')
+        }
+        else{
+            setTitleError('Title is too short!')
+        }
+    }
+    const handlePrice=e=>{
+        setPrice(e.target.value)
+        if(e.target.value>0){
+            setPriceError('')
+        }
+        else{
+            setPriceError('Price needs to be positive!')
+        }
+    }
+    const handleDescription=e=>{
+        setDescription(e.target.value)
+        if(e.target.value.length>2){
+            setDescriptionError('')
+        }
+        else{
+            setDescriptionError('Description is too short!')
+        }
+    }
 
     const onSubmitHandler = e =>{
         e.preventDefault();
@@ -20,11 +52,14 @@ export default ()=>{
         <form onSubmit={onSubmitHandler} className={style.flexer}>
                         <h1>Add a Product here!</h1>
             <label>Title:</label>
-            <input type='text' onChange={(e)=>{setTitle(e.target.value)}} value={title}/><br/>
+            <input type='text' onChange={(e)=>{handleTitle(e)}} value={title}/><br/>
+            <p>{titleError}</p>
             <label>Price:</label>
-            <input type='text' onChange={(e)=>{setPrice(e.target.value)}} value={price}/><br/>
+            <input type='text' onChange={(e)=>{handlePrice(e)}} value={price}/><br/>
+            <p>{priceError}</p>
             <label>Description:</label>
-            <input type='text' onChange={(e)=>{setDescription(e.target.value)}} value={description}/><br/>
+            <input type='text' onChange={(e)=>{handleDescription(e)}} value={description}/><br/>
+            <p>{descriptionError}</p>
             <input type='submit' value='Add Product'/>
         </form>   
     )
